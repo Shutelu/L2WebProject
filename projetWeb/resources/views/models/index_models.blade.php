462f9209c5a73220bbdf5e28b4b600f673b01237
Shutelu <changkai.wang@outlook.com>
2022-04-05 21:35:00 +0200

    v1.1.1 ajout de styles sur la barre de navigation

--- projetWeb/resources/views/models/index_models.blade.php
+++ projetWeb/resources/views/models/index_models.blade.php
@@ -7,7 +7,7 @@
         <title>@yield('title')</title>
 
         {{-- section pour l'ajout de style --}}
-        
+        <link rel="stylesheet" href="/styles/index_style.css">
         {{--ajout bootstrap--}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         
@@ -17,7 +17,7 @@
 
             {{-- Barre de navigation --}}
             <nav class="index-navigation">
-                <ul>
+                <ul id="index-navigation-ul">
                     <li><a href="{{route('pageIndex')}}">Accueil</a></li>
 
                     @guest
