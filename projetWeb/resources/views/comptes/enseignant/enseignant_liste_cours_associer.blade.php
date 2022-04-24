10ce2afd21bea1383b928d74ad8dbe3f1e6231c2
Shutelu <changkai.wang@outlook.com>
2022-04-24 17:00:00 +0200

    v1.4.5, fin 1.3 totaux de presence par cours

--- projetWeb/resources/views/comptes/enseignant/enseignant_liste_cours_associer.blade.php
+++ projetWeb/resources/views/comptes/enseignant/enseignant_liste_cours_associer.blade.php
@@ -3,11 +3,16 @@
 @section('title','Liste des cours qui vous sont associés')
 
 @section('content')
+
     <h1>Liste des cours associés</h1>
+
+    <?php $total = 0?>
+
     <table class="table-affichage-donnee">
         <tr>
             <th>Intitule</th>
             <th>Action</th>
+            <th>Totaux de présence du cours</th>
         </tr>
         @foreach ($liste_cours as $cours)
             <tr>
@@ -22,7 +27,33 @@
                         <button>Liste des séances pour ce cours</button>
                     </form>
                 </td>
+                <?php
+                    //on va chercher d'abord tout les etudiants du cours
+                    $liste_etudiants_pour_ce_cours = $cours->etudiants;
+                    foreach($liste_etudiants_pour_ce_cours as $etudiant){
+                        //puis tout les seances du cours
+                        foreach($cours->seances as $seance){
+
+                            $liste_present = $seance->etudiants;
+                            //on comparer comme on a fait dans la liste present absent
+                            foreach($liste_present as $present){
+
+                                if($present->id == $etudiant->id){
+                                    $total += 1;
+                                }
+                            }
+                        }
+                    }
+
+                    //marche mais avec juste un petit prob de +1 pour math introuvable | quantite 11 au lieu de 10
+                    // foreach($cours->seances as $seance){
+                    //     $total += $seance->etudiants()->count();
+                    // }
+
+                ?>
+                <td>{{$total}}</td>
             </tr>
+            <?php $total = 0?>
         @endforeach
     </table>
 @endsection
